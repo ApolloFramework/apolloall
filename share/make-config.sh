9b42d2d9005455e7c8c86781a1bc3d01e75689dd
Scott Kruger <scott.e.kruger@gmail.com>
2018-07-09 10:26:19 -0600

    Script for creating a spack-based config.sh
    Inspired by Nate's do-config.sh, this script creates a
    config.sh for configuring camellia2 by hand.  The sript assumes
    that spack has already installed camellia2 and then creates a
    configure script to be equivalent to spack's installed version.
    This method is more useful for development as one can tweak
    the configuration automatically and place it in a more
    convenient (non-hashed) location.

---
+++ share/make-config.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#set -e
+#BUILD_TYPE=Release
+BUILD_TYPE=RelWithDebInfo
+export INSTALL_PREFIX=${SCRATCH}/internal/camellia2
+script_name=config.sh
+
+
+
+# A Camellia do-config script that uses Spack-built dependencies
+# To find the dependencies, basic procedure is to 
+#   1. Use spack to build camellia
+#   2. Determine has of dependencies spack used in building camellia
+#   3. Use hash to determine location
+
+
+get_dep_loc () {
+
+   local pkg=$1
+
+   local hash=`spack find -dl camellia | grep $pkg | cut -f1 -d' '`
+   location=`spack location -i \/${hash}`
+}
+
+get_dep_loc mpi
+MPI_DIR=${location}
+
+get_dep_loc hdf5
+HDF5_DIR=${location}
+
+get_dep_loc trilinos
+TRILINOS_DIR=${location}
+
+get_dep_loc moab
+MOAB_DIR=${location}
+
+cat << EOF > ${script_name} 
+#!/bin/bash
+
+# Clear cmake cache to ensure a clean configure.
+rm -rf CMakeFiles CMakeCache.txt
+
+cmake \\
+  -D CMAKE_BUILD_TYPE="${BUILD_TYPE}" \\
+  -D CMAKE_INSTALL_PREFIX:PATH=${INSTALL_PREFIX} \\
+  -D CMAKE_INSTALL_RPATH_USE_LINK_PATH="ON" \\
+  -D CMAKE_COLOR_MAKEFILE:BOOL=FALSE \\
+  -D CMAKE_VERBOSE_MAKEFILE:BOOL=TRUE \\
+  -D CMAKE_C_COMPILER:FILEPATH='${MPI_DIR}/bin/mpicc' \\
+  -D CMAKE_CXX_COMPILER:FILEPATH='${MPI_DIR}/bin/mpicxx' \\
+  -D CMAKE_Fortran_COMPILER:FILEPATH='${MPI_DIR}/bin/mpif90' \\
+  -D Trilinos_ROOT_DIR:PATH=${TRILINOS_DIR} \\
+  -D ENABLE_MOAB:BOOL=ON \\
+  -D Moab_ROOT_DIR:PATH=${MOAB_DIR} \\
+  ${APOLLO_ROOT}/camellia2
+EOF
+chmod ug+rx ${script_name}
+
